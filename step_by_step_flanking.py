f4c52a01acca3637459d90f6cc28459c24710915
Anais Monroy-Eklund <monroyanais@gmail.com>
2016-07-01 14:58:31 -0400

    I finally made it functional with the addition of a new function. The output is scaffold, start, end

--- step_by_step_flanking.py
+++ step_by_step_flanking.py
@@ -4,6 +4,9 @@
 ## when do I make the script stop?
 ## should I only let it input those 12?
 
+# to run this code in Desktop/Projects/lncRNA directory on my laptop
+#python lncRNA_annotation_doc/step_by_step_flanking.py out/1_prac_ncRNA_and_genes.txt flybase/gene_orthologs_fb_2016_03.tsv Dsec
+
 
 #also, probably a good idea to turn list of tuples into a list of list...
 
@@ -34,6 +37,7 @@ def gff_list():
     return gff
       #gff_list ex/:
     #['2L', 'FlyBase', 'gene', '7529', '9484', '.', '+', '.', 'ID=FBgn0031208;Name=CG11023;Ontology_term=SO:0000010,SO:0000087,GO:0016929,GO:0016926;Dbxref=FlyBase:FBan0011023,FlyBase_Annotation_IDs:CG11023,GB_protein:ACZ94128,GB_protein:AAO41164,GB:AI944728,GB:AJ564667,GB_protein:CAD92822,GB:BF495604,UniProt/TrEMBL:Q86BM6,INTERPRO:IPR003653,GB_protein:AGB92323,UniProt/TrEMBL:M9PAY1,OrthoDB7_Drosophila:EOG796K1P,OrthoDB7_Diptera:EOG7X1604,EntrezGene:33155,UniProt/TrEMBL:E1JHP8,UniProt/TrEMBL:Q6KEV3,OrthoDB7_Insecta:EOG7Q8QM7,OrthoDB7_Arthropoda:EOG7R5K68,OrthoDB7_Metazoa:EOG7D59MP,InterologFinder:33155,BIOGRID:59420,FlyAtlas:CG11023-RA,GenomeRNAi:33155;gbunit=AE014134;derived_computed_cyto=21A5-21A5'], ['2L', 'FlyBase', 'gene', '9839', '21376', '.', '-', '.', 'ID=FBgn0002121;Name=l(2)gl;fullname=lethal (2) giant larvae;Alias=Lgl,lgl,lethal giant larvae,lethal giant larve,lethal giant larva,lethal(2)giant larvae,Complementation group 2.1,Lethal Giant Larvae,dlgl,p127l(2)gl,LGL,l(2) giant larva,CG2671,L(2)GL,p127,l(2)giant larvae,D-LGL,l(2),gl,l[[2]]gl,l-gl,lethal-giant-larvae,Lethal giant larvae,Lethal (2) giant larvae,L(2)gl,Lethal (2) giant larva,Lethal-giant-larvae,MENE (2L)-B,lethal(2) giant larvae,p127[l(2)gl],lethal(2)-giant larvae,lethal-2-giant larvae,l(2) giant larvae,lethal- giant-larvae,Lethal(2)giant larvae,Lethal-2-giant larvae;Ontology_term=SO:0000010,SO:0000087,GO:0005578,GO:0005886,GO:0007269,GO:0016082,GO:0008021,GO:0008283,GO:0016334,GO:0016336,GO:0016333,GO:0016335,GO:0016327,GO:0005829,GO:0045175,GO:0016332,GO:0045184,GO:0007399,GO:0005938,GO:0005737,GO:0007179,GO:0045197,GO:0045196,GO:0002009,GO:0005918,GO:0008105,GO:0045167,GO:0008104,GO:0045746,GO:0007423,GO:0008285,GO:0001738,GO:0016323,GO:0007391,GO:0005856,GO:0030154,GO:0042127,GO:0005614,GO:0045159,GO:0035072,GO:0007559,GO:0045200,GO:0008360,GO:0019991,GO:0007406,GO:0051726,GO:0051668,GO:0007314,GO:0016325,GO:0030036,GO:0030863,GO:0035070,GO:0055059,GO:0035212,GO:0035293,GO:0090163,GO:0048730,GO:0000132,GO:0098725,GO:0060429,GO:0007293,GO:0045176,GO:0072697,GO:0000149,SO:0000548,GO:0005920,GO:0017022,GO:0004860,GO:0006469;Dbxref=FlyBase:FBan0002671,FlyBase_Annotation_IDs:CG2671,INTERPRO:IPR015943,GB_protein:AAN10503,GB_protein:AAG22256,GB_protein:AAN10502,GB_protein:AAN10501,GB_protein:AAF51570,GB_protein:AAG22255,INTERPRO:IPR017986,GB:AA246243,GB:AW942062,GB:AY051654,GB_protein:AAK93078,GB:BH809482,GB:CZ471313,GB:CZ482024,GB:CZ484691,GB:M17022,GB_protein:AAA28671,GB_protein:AAA28672,GB:X05426,GB_protein:CAA29007,UniProt/Swiss-Prot:P08111,INTERPRO:IPR000664,INTERPRO:IPR001680,INTERPRO:IPR013577,GB_protein:AGB92324,UniProt/TrEMBL:M9NCX1,UniProt/TrEMBL:M9PBJ2,OrthoDB7_Drosophila:EOG7CW2GT,OrthoDB7_Diptera:EOG7DRVK2,GB_protein:AFH03479,GB_protein:AFH03478,GB_protein:AFH03481,GB_protein:AFH03480,EntrezGene:33156,INTERPRO:IPR013905,BDGP_clone:PC00404,OrthoDB7_Insecta:EOG7SRGKH,OrthoDB7_Arthropoda:EOG7ZDD82,OrthoDB7_Metazoa:EOG79W94C,InterologFinder:33156,FlyAtlas:CG2671-RB,BIOGRID:59421,Fly-FISH:CG2671,GenomeRNAi:33156,INTERACTIVEFLY:/cytoskel/lethl2g1.htm;gbunit=AE014134;derived_computed_cyto=21A5-21A5'],
+    # ['2L', 'FlyBase', 'ncRNA', '286383', '288292', '.', '+', '.', 'ID=FBtr0347595;Name=CR46263-RA;Parent=FBgn0267996;Dbxref=FlyBase_Annotation_IDs:CR46263-RA;score_text=Weakly Supported;score=0'], ['2L', 'FlyBase', 'gene', '287252', '289144', '.', '-', '.', 'ID=FBgn0025686;Name=Amnionless;fullname=Amnionless ortholog;Alias=FBgn0031246,CG11592,CK02467,BEST:CK02467,dAMN,Amnionless;Ontology_term=SO:0000010,SO:0000087,GO:0046331,GO:0097206,GO:0016021,GO:0097017;Dbxref=FlyBase:FBan0011592,FlyBase_Annotation_IDs:CG11592,GB_protein:AAF51514,GB:AA141784,GB:CZ468687,UniProt/TrEMBL:Q9VPN2,GB_protein:AGB92350,OrthoDB7_Drosophila:EOG7CGKJK,EntrezGene:33199,BDGP_clone:IP03221,OrthoDB7_Diptera:EOG774804,INTERPRO:IPR026112,OrthoDB7_Insecta:EOG7G266G,OrthoDB7_Arthropoda:EOG7P65FW,OrthoDB7_Metazoa:EOG7ZGX2W,InterologFinder:33199,FlyAtlas:CG11592-RA,GenomeRNAi:33199;gbunit=AE014134;derived_computed_cyto=21B7-21B7'], ['2L', 'FlyBase', 'gene', '292419', '293222', '.', '+', '.', 'ID=FBgn0031247;Name=CG11562;Alias=FBgn0063011,BcDNA:RE44650;Ontology_term=SO:0000010,SO:0000087,GO:0005739,GO:0003674,GO:0008150;Dbxref=FlyBase:FBan0011562,FlyBase_Annotation_IDs:CG11562,GB_protein:AAF51513,GB:AI520524,GB:AI945841,GB:AY119645,GB_protein:AAM50299,GB:BE662187,GB:BI358003,UniProt/TrEMBL:Q9VPN3,OrthoDB7_Drosophila:EOG7HTW3H,OrthoDB7_Diptera:EOG7200K9,EntrezGene:33200,BDGP_clone:RE44650,OrthoDB7_Insecta:EOG7B9454,OrthoDB7_Arthropoda:EOG7RK278,OrthoDB7_Metazoa:EOG78H3X3,FlyAtlas:CG11562-RA,INTERPRO:IPR031568,Fly-FISH:CG11562,GenomeRNAi:33200;gbunit=AE014134;derived_computed_cyto=21B7-21B7'], ['2L', 'FlyBase', 'gene', '292959', '294681', '.', '-', '.', 'ID=FBgn0017457;Name=U2af38;fullname=U2 small nuclear riboprotein auxiliary factor 38;Alias=FBgn0010626,U2AF38,U2AF,dU2AF38,DU2AF38,CG3582,dU2AF[38],l(2)06751,u2af38,U2AF 38;Ontology_term=GO:0089701,SO:0000010,SO:0000087,GO:0000398,GO:0008187,GO:0005681,GO:0005686,GO:0000381,GO:0005634,GO:0003729,GO:0007052,GO:0071011,GO:0008380,GO:0000166,GO:0046872;Dbxref=FlyBase:FBan0003582,FlyBase_Annotation_IDs:CG3582,GB_protein:AAF51512,GB:AA264081,GB:AA820431,GB:AC004115,GB:AC008371,GB:AI061776,GB:AI455418,GB:AI944553,GB:AQ026079,GB:AY058537,GB_protein:AAL13766,GB:U67066,GB_protein:AAB17271,UniProt/Swiss-Prot:Q94535,INTERPRO:IPR000504,INTERPRO:IPR000571,INTERPRO:IPR009145,INTERPRO:IPR012677,GB_protein:AGB92351,UniProt/TrEMBL:M9PBM1,OrthoDB7_Drosophila:EOG7FRM2M,OrthoDB7_Diptera:EOG700KS6,EntrezGene:33201,BDGP_clone:LD24048,OrthoDB7_Insecta:EOG76QSHP,OrthoDB7_Arthropoda:EOG7KMJ7T,OrthoDB7_Metazoa:EOG70089G,apodroso:10448-U2af38[k14504],InterologFinder:33201,FlyAtlas:CG3582-RA,BIOGRID:59457,Fly-FISH:CG3582,GenomeRNAi:33201;gbunit=AE014134;derived_computed_cyto=21B7-21B8']]
 
         
 #this works  
@@ -112,7 +116,7 @@ def ncRNA_gff_dict(rna_list, gff_list, window_length):
     #ncRNA_gff_dict
     #{'FBtr0336987': ('2L', '248909', '249812'), 'FBtr0309810': ('2L', '21952', '24237'), 'FBtr0345733': ('2L', '66318', '66524'), 'FBtr0345732': ('2L', '65999', '66242'), 'FBtr0344053': ('2L', '71039', '73836'), 'FBtr0344052': ('2L', '71039', '73836'), 'FBtr0347585': ('2L', '54817', '55767'), 'FBtr0347595': ('2L', '286383', '288292'), 'FBtr0347594': ('2L', '283807', '284255'), 'FBtr0336988': ('2L', '262203', '263003'), 'FBtr0336984': ('2L', '219651', '220050'), 'FBtr0336985': ('2L', '223519', '224708'), 'FBtr0336986': ('2L', '228037', '228588'), 'FBtr0344032': ('2L', '122837', '124031'), 'FBtr0336836': ('2L', '135402', '136128'), 'FBtr0336837': ('2L', '136144', '136701')}
 #this works!
-def mel_gene_set(dict):
+def mel_gene_set(dict): # this uses the flanking genes, specifically
     """This function finds unique mel genes, and puts them in a set (what is returned), so we don't get the same coords twice. It takes fbgn_id_dict. This is so we have the mel genes that we need coordinates for in the non-mel species', ie we're using this to find the orthologs that we care about"""
     mel_gene_set = set()
     for k, v in dict.iteritems():
@@ -127,7 +131,7 @@ def mel_gene_set(dict):
 def ortho_mapping(set):
     """This function maps other another species' orthologs to dmel genes"""
     mapping = dict()
-    with open(sys.argv[2], 'r') as orthos:
+    with open(sys.argv[2], 'r') as orthos: #this is finding the  ortho_gene_coords
         for line in orthos:
             if not line.startswith('#') and not line.startswith('\n'):
                 data = line.split ('\t')
@@ -170,50 +174,49 @@ def mel_rna_ortho(fbgn_id_dict, map):
     #'FBtr0309810': ([('FBgn0171619', 'scaffold_14', '27012', '28335'), ('FBgn0171620', 'scaffold_14', '19278', '25107'), ('FBgn0177629', 'scaffold_8', '1392069', '1396697')], [('FBgn0171618', 'scaffold_14', '31600', '58198')]), 
     #'FBtr0345733': ([('FBgn0171618', 'scaffold_14', '31600', '58198')], [('FBgn0171624', 'scaffold_14', '67886', '70036'), ('FBgn0171625', 'scaffold_14', '71736', '75558')]),
 
-def final_coord(ortho_dict, gff_dict):
+def final_coord(ortho_dict):#rna_ortho_dict,
     """This function finds the end of the front gene ortholog and the front of the back gene ortholog, to give a dictionary with a putative start and putative stop"""
     final_coord_dict = dict()
-    for key in ortho_dict.keys():
-        if key in gff_dict:
-            #up, down = [], []
-            #up_len= len(ortho_dict[key][0])
-            #print "This is up_len", up_len
-            #down_len = len(ortho_dict[key][1])
-            #print "This is down_len", down_len
+    for k, v in ortho_dict.iteritems():
+        upstream = v[0]
+        downstream = v[1]
+        uscafs = set()
+        dscafs = set()
+        for gene in upstream:
+            uscafs.add(gene[1])
+        for gene in downstream:
+            dscafs.add(gene[1])
+        #print "up", uscafs
+        #print "down", dscafs
+        common_scaf = uscafs.intersection(dscafs)
+        #print "common", common_scaf
+        for x in common_scaf:
+            #print "This is x", x
+            upos = []
+            for gene in upstream:
+                #print "This is gene", gene
+                if gene[1] == x:
+                    #print "This is gene[1]", gene[1]
+                    #print "This is common_scaf", common_scaf
+                    #upos.append(gene[0])
+                    upos.append(gene[2])
+                    upos.append(gene[3])
+                    #print upos
+            dpos = []
+            for gene in downstream:
+                if gene[1] == x:
+                    #dpos.append(gene[0])
+                    dpos.append(gene[2])
+                    dpos.append(gene[3])
+        #print "This is upos", upos
+        #print "This is dpos", dpos
+        print k, x, max(upos), min(dpos)
+    quit()
+        
+        #v[0], v[1] are lists of tuples
+        #FBtr0344032 ([('FBgn0171613', 'scaffold_14', '122003', '129535'), ('FBgn0171614', 'scaffold_14', '115786', '118485')], [('FBgn0171629', 'scaffold_14', '131505', '132704'), ('FBgn0171610', 'scaffold_14', '138261', '139727')])
 
-            if len(ortho_dict[key][0]) != 0 and len(ortho_dict[key][1]) != 0:
-                #print "This is upstream list", ortho_dict[key][0] , #type(ortho_dict[key])
-                #print "This is downstream list", ortho_dict[key][1]
-                for i in ortho_dict[key][0]: # problem is that [0] can change to as high as 4, depending on how many orthologs were found
-                    for j in ortho_dict[key][1]:
-                        if i[1] == j[1]:
-                            #print key, i, j
-                            break ## this breaks at the first instance of a match
-                        else:
-                            continue
-                print key, i, j
-            
-                
-                #combos = [zip(x, ortho_dict[key][0]) for x in itertools.permutations(ortho_dict[key][1], up_len)]
-                #print combos
-                #print type(combos)
-                #This is upstream list [('FBgn0171613', 'scaffold_14', '122003', '129535'), ('FBgn0171614', 'scaffold_14', '115786', '118485')] <type 'tuple'>
-                #This is downstream list [('FBgn0171629', 'scaffold_14', '131505', '132704'), ('FBgn0171610', 'scaffold_14', '138261', '139727')]
-                    ###
-            #[('FBgn0171613', 'scaffold_14', '122003', '129535'), ('FBgn0171614', 'scaffold_14', '115786', '118485')], [('FBgn0171629', 'scaffold_14', '131505', '132704'), ('FBgn0171610', 'scaffold_14', '138261', '139727')]
-
-                #up.append(ortho_dict[key][0][0][1])
-                #up.append(ortho_dict[key][0][0][3])
-            else:
-                print "skipped it all, either not upstream or no downstream!", orth_dict[key][0]
-                continue
-            
-            #if len(ortho_dict[key][1]) != 0:
-             #   down.append(ortho_dict[key][1][0][1])
-              #  down.append(ortho_dict[key][1][0][2])
-            
-            #final_coord_dict[key] = (up, down)
-    #return final_coord_dict     
+        #quit()
 ###PSEUDO CODE:::
 
 
@@ -221,7 +224,7 @@ def final_coord(ortho_dict, gff_dict):
 
 gff_obj =gff_list()
 #print gff_obj
-
+#quit()
 ncRNA_obj = ncRNA_list(gff_obj)
 #print ncRNA_obj
 #quit()
@@ -235,10 +238,10 @@ mel_genes_obj = mel_gene_set(fbgn_dict_obj)
 ortho_map = ortho_mapping(mel_genes_obj)
 #print ortho_map
 rna_ortho_dict = mel_rna_ortho(fbgn_dict_obj, ortho_map)
-print rna_ortho_dict
-quit()
+#print rna_ortho_dict
+#quit()
 #python lncRNA_annotation_doc/step_by_step_flanking.py out/1_prac_ncRNA_and_genes.txt 
-final_coord_obj = final_coord(rna_ortho_dict, ncRNA_gff_dict_obj)
+final_coord_obj = final_coord(rna_ortho_dict)
 #print final_coord_obj
 #print type(final_coord_obj)
 #write_out = open('2_%s_flanking_genes_%s_out.txt' %(fly,today),'w')
